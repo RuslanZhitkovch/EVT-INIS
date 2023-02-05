17ab3f6d97b355d3a38b73eb2f230c8ee33da879
Ruslan <rzhitkovich@bk.ru>
2023-02-05 22:19:50 +0300

    Сдела первую лабу INIS

---
+++ inis/lab1/script.js
@@ -0,0 +1,39 @@
+let numberOfFilms;
+
+while (isNaN(numberOfFilms) || numberOfFilms<1)
+{
+    numberOfFilms = prompt("Сколько фильмов вы посмотрели?");
+}
+
+
+const personalMovieDB= {
+    count: numberOfFilms,
+
+
+    movies: {
+    },
+}
+
+
+for (let i = 0; i < 2; i++)
+{
+    let question;
+    let answer;
+
+    while (question == null || question == '' || answer == '' || answer <=0 || answer == null || answer >10 || question.length > 50 )
+    {
+        question = prompt("Один из последних просмотренных фильмов?");
+        answer = prompt("Оцените этот фильм");
+
+    }
+
+    personalMovieDB["movies"][question] = answer;
+}
+
+
+
+
+
+
+
+console.log(personalMovieDB)
