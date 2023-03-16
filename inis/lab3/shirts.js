16b7efeea4d4c314d4d505a18b703cfd83bee41a
Ruslan <rzhitkovich@bk.ru>
2023-03-16 18:42:59 +0300

    First task of INIS-3 is ready. I need to make additional task with Quick-View

--- inis/lab3/shirts.js
+++ inis/lab3/shirts.js
@@ -1,8 +1,8 @@
 const shirts = [{
-    name: "Beep Boop",
-    description: "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
-    price: "$19.99",
-    colors: {
+    "name": "Beep Boop",
+    "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+    "price": "$19.99",
+    "colors": {
         "white": {
             "front": "shirt_images/beepboop-white-front.png",
             "back": "shirt_images/beepboop-white-back.png"
@@ -21,75 +21,103 @@ const shirts = [{
         }
     },
     "default": {
-        "front": "shirt_images/car-red-front.png",
-        "back": "shirt_images/default-m-back.png"
-    }
-},
-{
-    "name": "Car",
-    "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
-    "price": "$10.99",
-    "colors": {
-        "white": {
-            "front": "shirt_images/car-white-front.png",
-            "back": "shirt_images/car-white-back.png"
-        },
-        "blue": {
-            "front": "shirt_images/car-blue-front.png",
-            "back": "shirt_images/car-blue-back.png"
-        },
-        "green": {
-            "front": "shirt_images/car-green-front.png",
-            "back": "shirt_images/car-green-back.png"
-        },
-        "yellow": {
-            "front": "shirt_images/car-yellow-front.png",
-            "back": "shirt_images/car-yellow-back.png"
-        },
-        "red": {
-            "front": "shirt_images/car-red-front.png",
-            "back": "shirt_images/car-red-back.png"
-        }
-    },
-    "default": {
-        "front": "shirt_images/car-blue-front.png",
-        "back": "shirt_images/default-w-back.png"
-    }
-},
-{
-    "name": "Forever Plaid",
-    "price": "$13.99",
-    "description": "Proudly wear your tartan plaid as a patch on your front shirt pocket. And on the back, ask the important question that all CMU students should know the answer to: got plaid?",
-    "colors": {
-        "white": {
-            "front": "shirt_images/plaid-white-front.png",
-            "back": "shirt_images/plaid-white-back.png"
-        },
-        "pink": {
-            "front": "shirt_images/plaid-pink-front.png",
-            "back": "shirt_images/plaid-pink-back.png"
-        }
-    },
-    "default": {
-        "front": "shirt_images/car-blue-front.png",
-        "back": "shirt_images/default-w-back.png"
-    }
-},
-{
-    "name": "BSUIR",
-    "description": "BSUIR mission is to train engineers and scientists capable of generating and implementing innovative ideas, creating competitive high technology products in the spheres of computer science and electronics.",
-    "price": "$6.99",
-    "colors": {
-        "white": {
-            "front": "shirt_images/bsuir-white-front.png",
-            "back": "shirt_images/bsuir-white-back.png"
-        }
-    },
-    "default": {
         "front": "shirt_images/default-m-front.png",
         "back": "shirt_images/default-m-back.png"
     }
-}];
+},
+    {
+        "name": "Car",
+        "description": "As you move in to campus, one of the first memories so many students have is driving up to their dorm, unloading their bags, and moving in. How do they arrive to campus? By car, of course!",
+        "price": "$10.99",
+        "colors": {
+            "white": {
+                "front": "shirt_images/car-white-front.png",
+                "back": "shirt_images/car-white-back.png"
+            },
+            "blue": {
+                "front": "shirt_images/car-blue-front.png",
+                "back": "shirt_images/car-blue-back.png"
+            },
+            "green": {
+                "front": "shirt_images/car-green-front.png",
+                "back": "shirt_images/car-green-back.png"
+            },
+            "yellow": {
+                "front": "shirt_images/car-yellow-front.png",
+                "back": "shirt_images/car-yellow-back.png"
+            },
+            "red": {
+                "front": "shirt_images/car-red-front.png",
+                "back": "shirt_images/car-red-back.png"
+            }
+        },
+        "default": {
+            "front": "shirt_images/default-w-front.png",
+            "back": "shirt_images/default-w-back.png"
+        }
+    },
+    {
+        "name": "Forever Plaid",
+        "price": "$13.99",
+        "description": "Proudly wear your tartan plaid as a patch on your front shirt pocket. And on the back, ask the important question that all CMU students should know the answer to: got plaid?",
+        "colors": {
+            "white": {
+                "front": "shirt_images/plaid-white-front.png",
+                "back": "shirt_images/plaid-white-back.png"
+            },
+            "pink": {
+                "front": "shirt_images/plaid-pink-front.png",
+                "back": "shirt_images/plaid-pink-back.png"
+            }
+        },
+        "default": {
+            "front": "shirt_images/default-w-front.png",
+            "back": "shirt_images/default-w-back.png"
+        }
+    },
+    {
+        "name": "BSUIR",
+        "description": "BSUIR mission is to train engineers and scientists capable of generating and implementing innovative ideas, creating competitive high technology products in the spheres of computer science and electronics.",
+        "price": "$6.99",
+        "colors": {
+            "white": {
+                "front": "shirt_images/bsuir-white-front.png",
+                "back": "shirt_images/bsuir-white-back.png"
+            }
+        },
+        "default": {
+            "front": "shirt_images/default-m-front.png",
+            "back": "shirt_images/default-m-back.png"
+        },
+
+        "name": "Beep Boop",
+        "description": "Once upon a time, a mighty guide guarded the intersection of Forbes and Morewood, and would dutifully direct distracted college students when it was safe to cross the street. Its voice was soothing, strong, and steady. Its name was beep boop.",
+        "price": "$19.99",
+        "colors": {
+            "white": {
+                "front": "shirt_images/beepboop-white-front.png",
+                "back": "shirt_images/beepboop-white-back.png"
+            },
+            "blue": {
+                "front": "shirt_images/beepboop-blue-front.png",
+                "back": "shirt_images/beepboop-blue-back.png"
+            },
+            "pink": {
+                "front": "shirt_images/beepboop-pink-front.png",
+                "back": "shirt_images/beepboop-pink-back.png"
+            },
+            "red": {
+                "front": "shirt_images/beepboop-red-front.png",
+                "back": "shirt_images/beepboop-red-back.png"
+            }
+        },
+        "default": {
+            "front": "shirt_images/default-m-front.png",
+            "back": "shirt_images/default-m-back.png"
+        }
+
+
+    }];
 
 
 
@@ -98,26 +126,18 @@ var out = '';
 
 for (key in shirts)
 {
-
     out += '<div class = "single-shirt">';
-
-
-    console.log(shirts[key].default.front);
-
-    out +='<img src="'+shirts[key].default.front+'">';
-
-    out += 'Название: ' + shirts[key].name + '<br>';
-    out += 'Цена: ' + shirts[key].price + '<br>';
-    out += '<button>Hey</button>';
-    out += '<button>Her</button>';
+    out +='<img src="'+shirts[key].colors.white.front+'">';
+    out += '<h4>' + shirts[key].name + '</h4>';
+    out += '<p id = "count">'+ 'Availiable ' + Object.keys(shirts[key].colors).length + ' colors' +'</p>';
+    out += '<button>Quick View</button>';
+    out += '<button>See Page</button>';
     out +='</div>';
 
-
-
-
 }
-
 document.getElementById('out').innerHTML = out;
+
+
 
 
 
